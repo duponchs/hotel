6ba1c8e62f3496fdbad887a236ce4b729176cbbc
Stephane Duponchelle <stephane.duponchelle@hotmail.fr>
2020-08-24 14:16:16 +0200

    commit : Client et ClientDAOImpl et ServiceClient

--- src/main/java/fr/projethotel/core/DAO/ClientDAOImpl.java
+++ src/main/java/fr/projethotel/core/DAO/ClientDAOImpl.java
@@ -1,4 +1,28 @@
 package fr.projethotel.core.DAO;
 
+import fr.projethotel.core.HibernateUtil;
+import fr.projethotel.core.entity.Client;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class ClientDAOImpl {
+    public void create(Client client) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(client);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
--- src/main/java/fr/projethotel/core/entity/Client.java
+++ src/main/java/fr/projethotel/core/entity/Client.java
@@ -1,4 +1,48 @@
 package fr.projethotel.core.entity;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Client {
+    Integer id;
+    String nom;
+    String prenom;
+    Date dateNaissance;
+
+    public Client() {
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    @Column(name = "DATE_NAISSANCE")
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
 }
